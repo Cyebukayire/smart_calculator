aa04351a86556d4705586d8fa109ca0af8eb4421
Cyebukayire <peacecyebukayire@gmail.com>
2021-08-22 04:05:58 +0200

    Add JS Setup

---
+++ scritp.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelectorAll('[data-equals]')
+const deleteButton = document.querySelectorAll('[data-delete]')
+const allClearButton = document.querySelectorAll('[data-all-clear]')
+const previousOperandButton = document.querySelectorAll('[data-previous-operand]')
+const currentOperandButton = document.querySelectorAll('[data-current-operand]')
+
